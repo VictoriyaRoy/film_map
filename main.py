ff45d53ebe08c3108286a88007b7d8e940b52253
Victoriya Roi <victoriyaroi2003@gmail.com>
2021-02-15 18:17:13 +0200

    Count distance

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 import pandas as pd
 from geopy.geocoders import Nominatim
 from geopy.extra.rate_limiter import RateLimiter
@@ -104,12 +105,46 @@ def find_coordinates(films_data):
     return films_data
 
 
+def find_distance(lat1, lon1, lat2, lon2):
+    '''
+    Find distance between two points by coordinates
+    >>> round(find_distance(49.8177029, 24.0237912, 49.2204274, 28.3793954), -2)
+    321300.0
+    >>> find_distance(5.0, 600.0, 5.0, 600.0)
+    0.0
+    '''
+    radius = 6371e3
+    radian_lat1 = lat1 * math.pi/180
+    radian_lat2 = lat2 * math.pi/180
+    delta_lat = (lat2-lat1) * math.pi/180
+    delta_lon = (lon2-lon1) * math.pi/180
+
+    haversine = math.sin(delta_lat/2) * math.sin(delta_lat/2) + \
+            math.cos(radian_lat1) * math.cos(radian_lat2) * \
+            math.sin(delta_lon/2) * math.sin(delta_lon/2)
+    haversine = 2 * math.atan2(math.sqrt(haversine), math.sqrt(1-haversine))
+    haversine = radius * haversine
+
+    return haversine
+
+
+def count_distance_to_point(lat: float, lon: float, films_data: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Create new column 'Distance' which represented
+    distance between user point and points in dataframe
+    '''
+    films_data['Distance'] = films_data.apply(lambda x: \
+        find_distance(x['Latitude'], x['Longitude'], lat, lon), axis = 1)
+    return films_data
+
+
 def main():
     '''Main function'''
     films_data = read_file('locations.list')
     year = input('Please enter a year you would like to have a map for: ')
     films_data = choose_year(year, films_data)
     films_data = find_coordinates(films_data)
+    films_data = count_distance_to_point(49.8177029, 24.0237912, films_data)
     print(films_data)
 
 
